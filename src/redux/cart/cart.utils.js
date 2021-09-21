5cf883b9e95837128b4f2e66be3a1d249478bce3
Matt Layton <matthew.layton@cleverbridge.com>
2021-09-21 17:30:31 -0500

    Added addItem and removeItem functionality to CheckoutItem component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -20,3 +20,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   // Otherwise, add the item to the cart with a quantity of 1
   return [...cartItems, {...cartItemToAdd, quantity: 1}]
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  }
+
+  return cartItems.map(cartItem => 
+    cartItem.id === cartItemToRemove.id ? 
+    { ...cartItem, quantity: cartItem.quantity - 1 } : 
+    cartItem
+  )
+}

594408ff6c228d7655a9a2ffc1264a9c4b6a5ffd
Matt Layton <matthew.layton@cleverbridge.com>
2021-10-16 22:17:59 -0500

    Update Firebase admin SDK initialization for Heroku

--- server.js
+++ server.js
@@ -1,84 +1,90 @@
-import express from 'express'
-import path from 'path'
-import stripe from 'stripe'
-import dotenv from 'dotenv'
-import { ApolloServer } from 'apollo-server-express'
-import { typeDefs } from './gql/typeDefs.js'
-import { resolvers } from './gql/resolvers.js'
-import admin from 'firebase-admin'
+import express from "express";
+import path from "path";
+import stripe from "stripe";
+import dotenv from "dotenv";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs } from "./gql/typeDefs.js";
+import { resolvers } from "./gql/resolvers.js";
+import admin from "firebase-admin";
 
 // Dev dotenv
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config()
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
 }
 
 // Initialize Firebase Admin for backend access
 admin.initializeApp({
-  credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
-  databaseURL: "https://crwn-clothing-994f7.firebaseio.com"
-})
+  credential: admin.credential.cert(
+    process.env.NODE_ENV === "production"
+      ? JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+      : process.env.GOOGLE_APPLICATION_CREDENTIALS
+  ),
+  databaseURL: "https://crwn-clothing-994f7.firebaseio.com",
+});
 
 // Create instance of Firestore database
-const firestore = admin.firestore()
+const firestore = admin.firestore();
 
 // Create Stripe instance
-const stripeInstance = stripe(process.env.STRIPE_SECRET_KEY)
+const stripeInstance = stripe(process.env.STRIPE_SECRET_KEY);
 
 // Create Express server app
-const expressServer = express()
-const port = process.env.PORT || 5000
+const expressServer = express();
+const port = process.env.PORT || 5000;
 
-expressServer.use(express.json())
-expressServer.use(express.urlencoded({
-  extended: true
-}))
-
-if (process.env.NODE_ENV === 'production') {
-  expressServer.use(express.static(path.join(__dirname, 'client/build')))
-
-  expressServer.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+expressServer.use(express.json());
+expressServer.use(
+  express.urlencoded({
+    extended: true,
   })
+);
+
+if (process.env.NODE_ENV === "production") {
+  expressServer.use(express.static(path.join(__dirname, "client/build")));
+
+  expressServer.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
 // Start Apollo Server (GraphQL)
-let apolloServer = null 
+let apolloServer = null;
 const startApolloServer = async () => {
   apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => firestore
-  })
+    context: () => firestore,
+  });
 
   try {
-    await apolloServer.start()
-    apolloServer.applyMiddleware({ app: expressServer })
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app: expressServer });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-startApolloServer()
+startApolloServer();
 
 // Handle processing of Stripe payments
-expressServer.post('/payment', (req, res) => {
+expressServer.post("/payment", (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: 'usd'
-  }
+    currency: "usd",
+  };
 
   stripeInstance.charges.create(body, (stripeError, stripeResponse) => {
     if (stripeError) {
-      res.status(500).send({ error: stripeError })
+      res.status(500).send({ error: stripeError });
     } else {
-      res.status(200).send({ success: stripeResponse })
+      res.status(200).send({ success: stripeResponse });
     }
-  })
-})
+  });
+});
 
 // Start Express server
 expressServer.listen(port, (error) => {
-  if (error) throw error
-  console.log(`Express server running on port ${port}`)
-})
+  if (error) throw error;
+  console.log(`Express server running on port ${port}`);
+});
